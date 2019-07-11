7577ffb1c1feeeaf835a89bd7c9069793a217076
Martin <martinjoec@gmail.com>
2019-07-12 00:01:17 +0530

    deleted .idea

--- app/src/main/java/com/example/aacharya/StartActivity.java
+++ app/src/main/java/com/example/aacharya/StartActivity.java
@@ -3,15 +3,14 @@ package com.example.aacharya;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
     }
     public void login(View view){
         startActivity(new Intent(this,FeedActivity.class));
